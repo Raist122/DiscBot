7dc023a39fede6941f0161bf567764ba02a6fb72
Raist122 <raist122@hotmail.gr>
2017-10-19 14:53:13 +0300

    added discord commando

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
 
-bot.on('message', (message) => {
-    if (message.content == 'ping') {
-        message.reply('pong');
-    }
-});
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands")
+
 
 bot.login('MzcwMTY1MTg0Mjg0NzIxMTYz.DMjHXw.U4yQKkyO2y97bgGb4brX_D0f-JM');
